32256d3dbb47680c7d60f707a643aca8f83040e6
skramki <skramki@gmail.com>
2023-07-27 23:51:20 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_security_group" "allow_tls_custom" {
   }
 }
 
-# Step 5: Create EC2 Template
+# Step 5.a): Create EC2 Template
 resource "aws_launch_template" "aws-ec2-custom-template" {
   name = "aws-ec2-custom-template"
   key_name = "ec2-mgt-key"
@@ -132,7 +132,7 @@ resource "aws_launch_template" "aws-ec2-custom-template" {
 
 }
 
-# Step 5.a) Create Static EC2 Instance
+# Step 5.b) Create Static EC2 Instance
 resource "aws_instance" "appserver" {
   ami           = "ami-0acb5e61d5d7b19c8"
   instance_type = "t2.micro"
@@ -151,6 +151,7 @@ resource "aws_internet_gateway" "custom-internet-gw" {
     Name = "custom"
   }
 }
+
 # Step 7: Create Elastic IP for Custom
 resource "aws_eip" "custom-eip" {
   vpc = true
@@ -186,4 +187,123 @@ data "aws_nat_gateway" "custom-ngw" {
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateways.custom-internet-gw]
+}
+
+# Step 9: Custom Route Policy allow Public subnet to Internet Gateway
+
+resource "aws_route_table" "custom-routetb" {
+  vpc_id = aws_vpc.customvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gw.id
+  }
+
+  tags = {
+    Name = "custom-route-tb"
+  }
+}
+
+resource "aws_route_table_association" "custom-route-tb-ass" {
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.custom-routetb.id
+  //count = 2
+}
+
+// Adding NAT Gateway into the default Custom route table to Private subnet
+resource "aws_default_route_table" "defaultrtb" {
+  default_route_table_id = aws_vpc.customvpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.custom-ngw.id
+  }
+
+  tags = {
+    Name = "custom2default-route-tb"
+  }
+}
+
+# Step 10: Network ACL Ingress/Egress rules across custom VPC
+
+resource "aws_network_acl" "custom_nacl" {
+  vpc_id = aws_vpc.customvpc.id
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+  egress {  //Ephemeral Port
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "custom"
+  }
+}
+
+resource "aws_network_acl_association" "custom_nacl_ass" {
+  network_acl_id = aws_network_acl.custom_nacl.id
+
+  data "aws_subnets" "custom_subnets" {
+  filter {
+    name   = "tag:public-sub"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_subnet" "custom_subnet" {
+  for_each = toset(data.aws_subnets.custom_subnets.ids)
+  id       = each.value
+}
+
+  output "subnet_cidr_blocks" {
+    value = [for s in data.aws_subnet.custom_subnet : s.cidr_block]
+  }
+  tags = {
+    Name = "custom"
+  }
 }
